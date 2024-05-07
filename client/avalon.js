06b6fed1db6aa7b6d98d6b9e60097a7e358d9e57
NekoSuneVR <Chisdeal2014@hotmail.com>
2024-05-07 12:54:37 +0100

    Update avalon.js

--- client/avalon.js
+++ client/avalon.js
@@ -3,7 +3,7 @@ const javalon = require('javalon')
 if (localStorage.getItem('avalonAPI')) {
     javalon.init({api: localStorage.getItem('avalonAPI')})
 } else {
-    javalon.init({api: 'https://avalon.d.tube'})
+    javalon.init({api: 'https://s1dtube.nekosunevr.co.uk'})
 }
 
 Session.set('avalonAPI',javalon.config.api)
